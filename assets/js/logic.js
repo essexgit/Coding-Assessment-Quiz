f3fe9203125eb5a4e9be0921e2ee63cfea29d72a
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-01-13 10:07:06 +0000

    adding blank files and pseudo code

---
+++ assets/js/logic.js
@@ -0,0 +1,45 @@
+// A start button that when clicked a timer starts and the first question appears.
+
+// Listen for the start button being clicked on
+
+// Clicking the start button performs two functions:
+
+// The timer starts
+
+// Starting the timer performs two functions:
+
+// The start time is set
+
+// The timer counts down
+
+// If the timer equals zero, stop the game
+
+// The first questions appear
+
+// Get the question and print to the screen
+
+// List the answers and invite selection
+
+// Listen for a click on the answers
+
+// If the answer is clicked check if the answer is correct
+
+// Indicate if the answer is correct or not
+
+// If the answer is correct, add to the score
+
+// If the answer is not correct, reduce the time
+
+// Replace with the new question and answers
+
+// Questions contain buttons for each answer.
+
+// When answer is clicked, the next question appears
+
+// If there are no more questions, stop the game
+
+// If the answer clicked was incorrect then subtract time from the clock
+
+// The quiz should end when all questions are answered or the timer reaches 0.
+
+// When the game ends, it should display their score and give the user the ability to save their initials and their score;
