a2d4acd2a1df00bade01aea52c3c9a121ee9302b
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-01-14 18:33:14 +0000

    Timer and first set of Q&A

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,44 +1,97 @@
 // START BUTTON
-    // loads Q&A
-    // starts TIMER
+let start = document.querySelector("#start");
+let startButton = start.addEventListener("click", () => {
+    timer();
+    startGame();
+});
+
+function startGame() {
+    hiding();
+    showing();
+    loadQuestionTitle();
+    loadQuestionChoices();
+}
+// loads Q&A
+
+function loadQuestionTitle() {
+    let questContTitle = document.querySelector("#question-title");
+    questContTitle.textContent = currentQuestion = questions[0].title;
+}
+
+function loadQuestionChoices() {
+    //set choices container element
+    let questContChoices = document.querySelector("#choices");
+    // get choice array
+    let currentQuestion = questions[0].choices;
+    // create & append button
+    for (let i = 0; i < currentQuestion.length; i++) {
+        let choiceButton = document.createElement("button");
+        let choiceString = currentQuestion[i];
+        //set button text to choice
+        choiceButton.textContent = choiceString;
+        // set id to each choice for selection and comparison
+        choiceButton.setAttribute("id", `choice${i}`);
+        questContChoices.appendChild(choiceButton);
+    }
+}
 
 // TIMER function
-    // START TIMER
-        // max time value 
-        // countdown
-        // current time
-        
-    // CUT TIME
-        // cut value
-    // STOP TIMER
-        // If zero
-        // last question
-        // final time
+function timer() {
+    let startTime = 3;
+    let timeLeft = startTime;
+    // countdown
+    whatsTheTime = setInterval(function () {
+        let time = document.querySelector("#time");
+        timeLeft--;
+        time.textContent = timeLeft;
+
+        if (timeLeft === 0) {
+            clearInterval(whatsTheTime);
+        }
+    }, 1000);
+}
+
+
+// CUT TIME
+// cut value
+// STOP TIMER
+// If zero
+// last question
+// final time
+
 
 // ANSWER function
-    // if correct
-        // next
-        
-    // if wrong
-     // show correct
-     // deduct time
-     // next
+// choose
+function choosing() {
+    // loop buttons adding listener
+    // let choice = 
+}
+// if correct
+// next
+
+// if wrong
+// show correct
+// deduct time
+// next
 
 // DISPLAY functions
-    //  Start Group
-        //  submit
-        //  instructions
-    // Run Group
-        // choices
-        // chosen
-        // question
-        // countdown
-        // answers
-    // End Group
-        // result
-        // score
-        // instructions
+// Run Group
+// hiding, start button,instruction
+function hiding() {
+    let startScreen = document.querySelector("#start-screen");
+    startScreen.classList.add("hide");
+}
+// showing,question, choices, chosen, countdown, answers
+function showing() {
+    let questionScreen = document.querySelector("#questions");
+    questionScreen.classList.remove("hide");
+}
+// End Group
+    // result
+    // score
+    // instructions
+    // start button display
 
 // HIGHSCORE function add
     // add initial to score reference
-       // add to local storage
+    // add to local storage
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,4 +4,34 @@ var questions = [
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
+    {
+        title: "Javascript is an _______ language?",
+        choices: ["Object-Oriented", "Object-Based", "Procedural", "Declarative "],
+        answer: "Object-Oriented"
+    },
+    {
+        title: "What does the ‘toLocaleString()’ method do in JS?",
+        choices: ["Returns a localised object representation", "Returns a parsed string", "Returns a localised string representation of an object", "None of the above"],
+        answer: "Returns a localised string representation of an object"
+    },
+    {
+        title: "Which of the following methods is used to access HTML elements using Javascript?",
+        choices: ["getElementbyID()", "getElementsByClassName()", "Both of the above", "None of the above"],
+        answer: "Both of the above"
+    },
+    {
+        title: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        choices: ["Throws an error", "Ignores the statements", "Gives a Warning", "None of the Above"],
+        answer: "Ignores the statements"
+    },
+    {
+        title: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        choices: ["Boolean", "Undefined", "Object", "Integer"],
+        answer: "Object"
+    },
+    {
+        title: "What keyword is used to check whether a given property is valid or not?",
+        choices: ["in", "is in", "exists", "lies"],
+        answer: "in"
+    },
 ];
