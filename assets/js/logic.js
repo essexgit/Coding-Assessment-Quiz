bb9284f962f55a6b763fdee7ed6dcfa45a6f7520
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-01-17 23:45:29 +0000

    submission upload

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,6 +16,7 @@ let whatsTheTime;
 let startTime = 64;
 let time = document.querySelector("#time"); // referencing time element
 
+
 let startButton = start.addEventListener("click", () => {
     timer();
     startGame();
@@ -44,6 +45,8 @@ function loadQuestionTitle(questCounter) {
 // showing, question, choices, chosen, countdown, answers
 function showing() {
     questionScreen.classList.remove("hide");
+    feebackDiv.classList.add('hide');
+    feebackDiv.textContent = '';
 }
 function loadQuestionChoices(questCounter) {
     // get choice array
@@ -88,6 +91,7 @@ function choosing(questCounter) {
 }
 
 function checking(answerText, answerID, questCounter) {
+    feebackDiv.classList.remove('hide');
     if (answerText === questions[questCounter].answer) {
         correctAnswer(answerID, questCounter);
     } else {
@@ -97,12 +101,14 @@ function checking(answerText, answerID, questCounter) {
 }
 
 function correctAnswer(answerID, questCounter) {
+    feebackDiv.textContent = 'Correct';
     let tickBox = document.querySelector(`#${answerID}`);
     tickBox.classList.add("bggreen");
     nextQuestion(questCounter);
 }
 
 function wrongAnswer(correctChoiceIndex, answerID, questCounter) {
+    feebackDiv.textContent = 'Wrong';
     let tickBox = document.querySelector(`#choice${correctChoiceIndex}`);
     let wrongBox = document.querySelector(`#${answerID}`);
     tickBox.classList.add("bggreen");
@@ -113,7 +119,6 @@ function wrongAnswer(correctChoiceIndex, answerID, questCounter) {
 
 function nextQuestion(questCounter) {
     nextBtn.classList.remove("hide");
-
 }
 
 
@@ -133,9 +138,25 @@ function clearQuestions() {
 function endGame() {
     clearInterval(whatsTheTime);
     nextBtn.classList.add('hide');
-
+    endScreen.classList.remove('hide');
+    finalScore.textContent = startTime;
 }
 
+submitBtn.addEventListener("click", () => {
+    // final score submit object
+    let scoreObj = [];
+    scoreObj = JSON.stringify([{
+        gameScore: startTime,
+        gameInitials: initialEnt.value
+    }]);
+    let storedScores = localStorage.getItem('scoreObj');
+    if (storedScores !== null) {
+        scoreObj += storedScores;
+    }
+
+    localStorage.setItem('scoreObj', scoreObj);
+    location.reload();
+});
 // End Group
     // result
     // score
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,7 +1,20 @@
 // HIGHSCORE function manage
-    // PAGE
-        // on load, update from local storage
-        // rank by score 
+let highScoreList = document.querySelector("#highscores");
+let clearHighScore = document.querySelector("#clear");
+let highScoreStore = JSON.parse(localStorage.getItem("scoreObj"));
+let li = document.createElement("li");
+// PAGE
+// on load, update from local storage
+for (const gameScore in highScoreStore) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+
+    }
+}
+highScoreList.app;
+
+
+        // rank by score
     //  clear highscores button
         // delete highscores from local storage
         // upload from local storage
